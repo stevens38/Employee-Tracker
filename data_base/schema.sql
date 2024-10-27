43d1025e90a2869b04f0c8331a999bcdb8de191f
JStevens <stevens38@marshall.edu>
2024-10-27 18:59:25 -0400

    tables working

--- data_base/schema.sql
+++ data_base/schema.sql
@@ -2,21 +2,20 @@ Drop database if exists employee_db;
 create database employee_db;
 \c employee_db;
 create table department(
-    id int auto_increment primary key,
+    id serial primary key,
     name varchar(30)
 );
 create table role(
-    id int auto_increment primary key,
+    id serial primary key,
     title varchar(30),
     salary decimal(10,2),
-    department_id int,
-    foreign key(department_id) references department(id)
+    department_id integer not null references department(id)
 );
 create table employee(
-    id int auto_increment primary key,
+    id serial primary key,
     first_name varchar(30),
     last_name varchar(30),
-    foreign key(role_id) references role(id),
-    foreign key(manager_id) references employee(id)
+    role_id integer not null references role(id),
+    manager_id integer references employee(id) on delete set null
 );
 
