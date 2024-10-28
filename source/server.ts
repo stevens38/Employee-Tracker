2859c639dc2c79671d6626ba8b819ecdd91fe36d
JStevens <stevens38@marshall.edu>
2024-10-27 20:39:14 -0400

    package.json update and server.ts in progress

--- source/server.ts
+++ source/server.ts
@@ -1,10 +1,25 @@
 import express from 'express';
-import inqyirer from 'inquirer';
-import {pool, connectToDb} from './connections.js';
+import inquirer from 'inquirer';
+import {pool, comectionToDb} from './connections.js';
 
 await connectToDb();
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = 3001;
 
-app.use(express.urencoded({extended: false}));
+app.use(express..urlencoded({ extended: true }));
+app.use(express.json());
+
+inquirer
+    .prompt([
+        {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: ['Add a department', 'Add a role', 'Add an employee', 'View departments', 'View roles', 'View employees', 'Update an employee role'],
+        },
+    ])
+    .then((answers) => {
+        console.log(answers);
+    });
+
